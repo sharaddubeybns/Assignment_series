4dbcd4711d511d9ae88578fab5bb94279d590092
sharaddubeybns <125786148+sharaddubeybns@users.noreply.github.com>
2025-04-17 23:07:58 +0530

    Delete ASSIGNMENT 5 directory

--- ASSIGNMENT 5/Task-1/Problem1.py
+++
@@ -1,7 +0,0 @@
-# Creating the dictionary
-student_marks = {
-    "Alice": 85,
-}
-
-# Printing the dictionary
-print(student_marks)
--- ASSIGNMENT 5/Task-2/Problem2.py
+++
@@ -1,8 +0,0 @@
-# Create a list of numbers from 1 to 10
-numbers = list(range(1, 11))
-
-# Extract the first five elements
-first_five = numbers[:5]
-
-# Display the result
-print("First five elements:", first_five)
